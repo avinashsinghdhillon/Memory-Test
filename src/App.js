8e1435a196bc227d742906ecc850cb2540580b4a
Avinash Singh <avinash.dhillon@gmail.com>
2019-07-28 16:03:17 -0500

    created and linked component files to app.js. No content yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Gameboard from "./components/Gameboard"
+import Clicky from "./components/Clicky";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      {/* <Header />
+      <Gameboard>
+        This is a child
+        <Clicky />
+      </Gameboard>
+      <Footer /> */}
     </div>
   );
 }
